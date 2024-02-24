23288ba989fe7a7c12ebf76929bc2420d3dc51b8
Ashy5000 <ashydashy5000@gmail.com>
2024-02-25 08:19:46 +0900

    Implements basic Rust peer list server

---
+++ peer_server/src/main.rs
@@ -0,0 +1,28 @@
+use actix_web::{get, web, App, HttpServer, Responder};
+
+#[get("/add_peer/{ip}")]
+async fn collect_peer_list(ip: web::Path<String>) -> impl Responder {
+    // Add the peer to the list of peers in peers.txt
+    std::fs::write("peers.txt", ip.to_string() + "\n").unwrap();
+    println!("Peer added: {}", ip);
+    format!("Peer added: {}", ip)
+}
+
+#[get("/get_peers")]
+async fn serve_peer_list() -> impl Responder {
+    // Read the list of peers from peers.txt
+    let peers = std::fs::read_to_string("peers.txt").unwrap();
+    peers
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(collect_peer_list)
+            .service(serve_peer_list)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
