6e47e569610d01fbc0b37505ae7a4c029379835b
Ashy5000 <ashydashy5000@gmail.com>
2024-02-26 19:28:59 -0800

    Implements fetching peers from remote server

--- main.go
+++ main.go
@@ -4,10 +4,13 @@ import (
 	"flag"
 )
 
+var useLocalPeerList *bool
+
 func main() {
 	mine := flag.Bool("mine", false, "Set to true to start node as miner")
 	serve := flag.Bool("serve", *mine, "Set to true to start node as server")
 	port := flag.String("port", "8080", "Port to listen on (server only)")
+	useLocalPeerList = flag.Bool("useLocalPeerList", false, "Set to true to use local peer list")
 	flag.Parse()
 	if *serve {
 		Serve(*mine, *port)
--- peers.go
+++ peers.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"bufio"
+	"io"
+	"net/http"
 	"os"
+	"strings"
 )
 
 func AddPeer(ip string) {
@@ -24,23 +27,39 @@ func AddPeer(ip string) {
 }
 
 func GetPeers() []string {
-	file, err := os.Open("peers.txt")
-	if err != nil {
-		panic(err)
-	}
-	defer func(file *os.File) {
-		err := file.Close()
+	if *useLocalPeerList {
+		file, err := os.Open("peers.txt")
 		if err != nil {
 			panic(err)
 		}
-	}(file)
+		defer func(file *os.File) {
+			err := file.Close()
+			if err != nil {
+				panic(err)
+			}
+		}(file)
 
-	scanner := bufio.NewScanner(file)
+		scanner := bufio.NewScanner(file)
 
-	var result []string
-	for scanner.Scan() {
-		result = append(result, scanner.Text())
+		var result []string
+		for scanner.Scan() {
+			result = append(result, scanner.Text())
+		}
+
+		return result
+	} else {
+		peerServer := "http://192.168.4.8"
+		// Send a request to the peer server to get the list of peers
+		res, err := http.Get(peerServer + "/peers")
+		if err != nil {
+			panic(err)
+		}
+		body, err := io.ReadAll(res.Body)
+		if err != nil {
+			panic(err)
+		}
+
+		// Split result on newline
+		return strings.Split(string(body), "\n")
 	}
-
-	return result
 }
