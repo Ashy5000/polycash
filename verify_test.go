204965b88342cbb8ba687cd6f952a2badeafe4d9
Ashy5000 <ashydashy5000@gmail.com>
2024-03-14 20:18:23 -0700

    Implements testing for VerifyBlock()

--- verify_test.go
+++ verify_test.go
@@ -56,3 +56,60 @@ func TestVerifyTransaction(t *testing.T) {
 		assert.False(t, result)
 	})
 }
+
+func TestVerifyBlock(t *testing.T) {
+	t.Run("It should return true if the block is valid", func(t *testing.T) {
+		key := GetKey()
+		blockchain = nil
+		Append(GenesisBlock())
+		sender := key.PublicKey
+		receiver := key.PublicKey
+		amount := "0"
+		r := big.NewInt(0)
+		s := big.NewInt(0)
+		hash := sha256.Sum256([]byte(fmt.Sprintf("%s:%s:%s", sender.Y, receiver.Y, amount)))
+		r, s, err := dsa.Sign(rand.Reader, &key, hash[:])
+		if err != nil {
+			panic(err)
+		}
+		block := Block{
+			Sender:            sender,
+			Recipient:         receiver,
+			Amount:            0,
+			R:                 *r,
+			S:                 *s,
+			PreviousBlockHash: HashBlock(GenesisBlock()),
+			Miner:             key.PublicKey,
+			Difficulty:        1,
+		}
+		result := VerifyBlock(block)
+		assert.True(t, result)
+	})
+	t.Run("It should return false if the block has an invalid transaction", func(t *testing.T) {
+		key := GetKey()
+		blockchain = nil
+		Append(GenesisBlock())
+		sender := key.PublicKey
+		receiver := key.PublicKey
+		amount := "1"
+		r := big.NewInt(0)
+		s := big.NewInt(0)
+		hash := sha256.Sum256([]byte(fmt.Sprintf("%s:%s:%s", sender.Y, receiver.Y, amount)))
+		r, s, err := dsa.Sign(rand.Reader, &key, hash[:])
+		if err != nil {
+			panic(err)
+		}
+		block := Block{
+			Sender:            sender,
+			Recipient:         receiver,
+			Amount:            0,
+			R:                 *r,
+			S:                 *s,
+			PreviousBlockHash: HashBlock(GenesisBlock()),
+			Miner:             key.PublicKey,
+			Difficulty:        1,
+		}
+		result := VerifyBlock(block)
+		assert.False(t, result)
+	})
+}
