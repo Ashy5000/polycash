eba99b6b00f3acca1418a667af6e124034945b28
Ashy5000 <ashydashy5000@gmail.com>
2024-03-27 18:44:42 -0700

    Updates testify package and fixes snake case

--- difficulty.go
+++ difficulty.go
@@ -13,15 +13,15 @@ import (
 	"time"
 )
 
-func GetDifficulty(last_time time.Duration, last_difficulty uint64) uint64 {
+func GetDifficulty(lastTime time.Duration, lastDifficulty uint64) uint64 {
 	// The target time for a block is 1 minute.
 	// The difficulty is adjusted on a per-miner, per-block basis.
 	// To give faster miners a (small) advantage, the difficulty is divided by the result of a modified sigmoid function.
 	// It looks like this:
 	// 1 / (1 + e^(-(x-mdpm)/mdpm))
 	// Where x is the previous time times the previous difficulty, and 1 mdpm is  1000000 (1 million) difficulty points per minute.
-	difficultyBeforeAdjustment := last_difficulty * uint64(60) / uint64(last_time.Seconds())
-	x := last_time.Minutes() * float64(last_difficulty)
+	difficultyBeforeAdjustment := lastDifficulty * uint64(60) / uint64(lastTime.Seconds())
+	x := lastTime.Minutes() * float64(lastDifficulty)
 	adjustment := (1 / (1 + math.Pow(math.E, -(1/mdpm)*(x-mdpm)))) + 0.5
 	difficultyAfterAdjustment := float64(difficultyBeforeAdjustment) / adjustment
 	return uint64(difficultyAfterAdjustment)
