40a7b6a464b598f3a78e18d5dc7a6637ecf03354
Ashy5000 <ashydashy5000@gmail.com>
2024-04-01 11:08:48 -0700

    Adds missing bootstrap.go

---
+++ bootstrap.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func Bootstrap() {
+	// Connect to all peers' peers
+	peers := GetPeers()
+	for _, peer := range peers {
+		// Get the peer's peers
+		req, err := http.NewRequest(http.MethodGet, peer+"/peers", nil)
+		if err != nil {
+			panic(err)
+		}
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			fmt.Println("Peer is down.")
+			continue
+		}
+		peerPeersBytes, err := io.ReadAll(resp.Body)
+		if err != nil {
+			panic(err)
+		}
+		var peerPeers []string
+		err = json.Unmarshal(peerPeersBytes, &peerPeers)
+		for _, peerPeer := range peerPeers {
+			// Add the peer's peers to the list of peers
+			AddPeer(peerPeer)
+		}
+	}
+}
