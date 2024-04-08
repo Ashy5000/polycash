c269caec02ca58994704696fdbd59f957057aee5
Ashy5000 <ashydashy5000@gmail.com>
2024-04-08 07:20:29 -0700

    Adds missing license notices

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-# This script is used to build the project
+# Copyright 2024, Asher Wrobel
+
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 # Build all combinations of os and arch
 for os in linux darwin windows; do
