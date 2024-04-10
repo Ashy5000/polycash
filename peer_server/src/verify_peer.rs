1609d473f51655ae0b3f392d73101c8473f5a39a
Ashy5000 <ashydashy5000@gmail.com>
2024-04-10 16:21:47 -0700

    Fixes issues with peer server

--- peer_server/src/verify_peer.rs
+++ peer_server/src/verify_peer.rs
@@ -11,10 +11,10 @@ use std::net::IpAddr;
 use std::str::FromStr;
 use std::process::Command;
 
-pub fn verify_peer(ip: &web::Path<String>) -> bool {
+pub fn verify_peer(ip: &String) -> bool {
     // First, check if the IP address is valid
     // This also serves as a sanitizer which is important as we are going to use the IP address in a command
-    let addr_result = IpAddr::from_str(&ip.to_string());
+    let addr_result = IpAddr::from_str(&ip);
     if addr_result.is_err() {
         return false;
     }
@@ -22,7 +22,7 @@ pub fn verify_peer(ip: &web::Path<String>) -> bool {
     let ping = Command::new("ping")
         .arg("-c")
         .arg("1")
-        .arg(ip.to_string())
+        .arg(ip)
         .output()
         .expect("failed to execute process");
     if !ping.status.success() {
@@ -31,7 +31,7 @@ pub fn verify_peer(ip: &web::Path<String>) -> bool {
     // Next, check if the IP address is already in the list of peers
     let peers = std::fs::read_to_string("peers.txt").unwrap();
     for peer in peers.lines() {
-        if peer == ip.to_string() {
+        if peer == ip {
             return false;
         }
     }
