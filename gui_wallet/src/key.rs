937465a79525ef93834ce05ea23faa37de89ffc5
Ashy5000 <ashydashy5000@gmail.com>
2024-04-14 16:46:17 -0700

    Updates GUI wallet to work with Dilithium2 signatures

--- gui_wallet/src/key.rs
+++ gui_wallet/src/key.rs
@@ -6,27 +6,25 @@ This program is distributed in the hope that it will be useful, but WITHOUT ANY 
 
 You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
 */
-use std::fs;
 use std::process::Command;
-use regex::Regex;
 
 #[derive(Debug)]
 pub(crate) struct PublicKeyError {}
 
 pub(crate) fn get_public_key() -> Result<String, PublicKeyError> {
-    let key_string_result = fs::read_to_string("../key.json");
-    let key_string = match key_string_result {
-        Ok(key_string) => key_string,
-        Err(..) => {
-            return Err(PublicKeyError {})
-        }
-    };
-    let re = Regex::new(r"Y(.):(?<y>.*),").unwrap();
-    let Some(caps) = re.captures(&key_string) else {
-        println!("no match!");
-        return Ok("".to_string())
-    };
-    Ok(caps["y"].to_string())
+    let output = Command::new("env")
+        .arg("-C")
+        .arg("..")
+        .arg("./builds/node/node_linux-amd64")
+        .arg("-command")
+        .arg("getpubkey")
+        .output()
+        .expect("Failed to run node executable");
+    let s = match String::from_utf8(output.stdout) {
+        Ok(v) => v,
+        Err(_) => return Err(PublicKeyError{}),
+};
+    return Ok(s)
 }
 
 pub(crate) fn generate_key() {
