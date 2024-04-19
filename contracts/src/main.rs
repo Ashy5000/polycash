ef67aa088c3264a39785218c41df82b4e0b91de1
Ashy5000 <ashydashy5000@gmail.com>
2024-04-19 16:48:51 -0700

    Implements syntax tree parsing

--- contracts/src/main.rs
+++ contracts/src/main.rs
@@ -1,6 +1,10 @@
 mod read_contract;
+mod syntax_tree;
 
 fn main() {
     let contract_contents = read_contract::read_contract();
-    println!("Contract contents:\n{}", contract_contents);
+    println!("{}", contract_contents);
+    let mut tree = syntax_tree::build_syntax_tree();
+    tree.create(contract_contents);
+    println!("{:?}", tree);
 }
---
+++ contracts/src/syntax_tree.rs
@@ -0,0 +1,54 @@
+#[derive(Debug)]
+pub(crate) struct Line {
+    pub(crate) command: String,
+    pub(crate) args: Vec<String>,
+}
+
+pub(crate) fn build_line() -> Line {
+    Line {
+        command: "".to_owned(),
+        args: Vec::new(),
+    }
+}
+
+#[derive(Debug)]
+pub(crate) struct SyntaxTree {
+    pub(crate) lines: Vec<Line>
+}
+
+impl SyntaxTree {
+    pub(crate) fn create(&mut self, contract_contents: String) {
+        let asm_lines_iter = contract_contents.split("\n");
+        for asm_line in asm_lines_iter {
+            let parts_iter = asm_line.split(" ");
+            let mut parts = Vec::new();
+            for mut part in parts_iter {
+                if part == ";".to_owned() {
+                    break
+                }
+                let part_chars = &part.chars();
+                let first_two_chars: String = part_chars.to_owned().into_iter().take(2).collect();
+                let part_string;
+                if first_two_chars == "0x".to_owned() {
+                    part_string = part_chars.to_owned().into_iter().skip(2).take(part.len() - 2).collect::<String>().to_owned();
+                    part = part_string.as_str();
+                }
+                parts.push(part.to_owned())
+            }
+            if parts.len() == 0 {
+                continue
+            }
+            let mut line = build_line();
+            line.command = parts[0].to_owned();
+            let args = parts.split_off(1);
+            line.args = args;
+            self.lines.push(line);
+        }
+    }
+}
+
+pub(crate) fn build_syntax_tree() -> SyntaxTree {
+    SyntaxTree {
+        lines: Vec::new(),
+    }
+}
