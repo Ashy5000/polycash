e1ac2400f56650ff7ddcda20cf49c02bb96f55d6
Ashy5000 <ashydashy5000@gmail.com>
2024-04-20 06:50:25 -0700

    Adds testing for vm_throw_global_error() and vm_throw_local_error()

--- contracts/tests/vm_test.rs
+++ contracts/tests/vm_test.rs
@@ -18,4 +18,31 @@ mod vm_test {
         let initialization = contracts::vm::vm_check_buffer_initialization(&mut buffers, "12341234".to_owned());
         assert!(initialization);
     }
+
+    #[test]
+    fn test_vm_throw_global_error() {
+        let mut buffers: HashMap<String, Buffer> = HashMap::new();
+        buffers.insert(
+            "00000000".to_owned(),
+            Buffer { contents: Vec::new() }
+        );
+        contracts::vm::vm_throw_global_error(&mut buffers);
+        let mut found_buffer = false;
+        if let Some(x) = buffers.get("00000000") {
+            found_buffer = true;
+            assert_eq!(x.contents.len(), 1);
+            assert_eq!(x.contents[0], 1);
+        }
+        assert!(found_buffer);
+    }
+
+    #[test]
+    fn test_vm_throw_local_error() {
+        let mut buffers: HashMap<String, Buffer> = HashMap::new();
+        buffers.insert(
+            "12341234".to_owned(),
+            Buffer { contents: Vec::new() }
+        );
+        contracts::vm::vm_throw_local_error(&mut buffers, "12341234".to_owned())
+    }
 }
