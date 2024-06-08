c2c15e3cfb33619e7a189da0e047a7c67afa678b
Ashy5000 <ashydashy5000@gmail.com>
2024-06-08 11:19:20 -0700

    Changes file extension again (.pl is used for Perl)

--- language/src/main.cpp
+++ language/src/main.cpp
@@ -13,7 +13,7 @@
 int main(int argc, char* argv[]) {
     if (argc != 2) {
         std::cerr << "Incorrect usage." << std::endl;
-        std::cerr << "plc [in.pl]" << std::endl;
+        std::cerr << "plc [in.poly]" << std::endl;
     }
 
     std::stringstream contents_stream;
