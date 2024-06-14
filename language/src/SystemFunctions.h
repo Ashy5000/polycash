f32651467984865f1e057cca6271b9c9dcfa3dae
Ashy5000 <ashydashy5000@gmail.com>
2024-06-14 08:53:31 -0700

    Migrates all system functions into functions vector

--- language/src/SystemFunctions.h
+++ language/src/SystemFunctions.h
@@ -30,6 +30,105 @@ const std::vector SYSTEM_FUNCTIONS = {
         },
         "contract",
         "exit"
+    ),
+    SystemFunction(
+        [](const std::vector<Token>& params, int &nextAllocatedLocation, std::vector<Variable> &vars) -> std::string {
+            std::stringstream blockasm;
+            std::string typeString = params[1].children[0].value;
+            Type type = Type::type_placeholder;
+            if(typeString == "uint64") {
+                type = Type::uint64;
+            }
+            if(type == Type::type_placeholder) {
+                std::cerr << "Unknown type " << typeString << std::endl;
+                exit(EXIT_FAILURE);
+            }
+            auto var = Variable(params[0].children[0].value, nextAllocatedLocation, type);
+            vars.emplace_back(var);
+            blockasm << "InitBfr 0x" << std::setfill('0') << std::setw(8) << std::hex << nextAllocatedLocation++ << " 0x00000000" << std::endl;
+            return blockasm.str();
+        },
+        "memory",
+        "alloc"
+    ),
+    SystemFunction(
+        [](const std::vector<Token>& params, int &nextAllocatedLocation, std::vector<Variable> &vars) -> std::string {
+            std::stringstream blockasm;
+            int indexToRemove = -1;
+            for(int j = 0; j < vars.size(); j++) {
+                if(Variable var = vars[j]; var.name == params[0].children[0].value) {
+                    indexToRemove = j;
+                    break;
+                }
+            }
+            if(indexToRemove == -1) {
+                std::cerr << "Cannot free undefined variable." << std::endl;
+                exit(EXIT_FAILURE);
+            }
+            blockasm << "Free 0x" << std::setfill('0') << std::setw(8) << std::hex << vars[indexToRemove].location << " 0x00000000" << std::endl;
+            vars.erase(vars.begin() + indexToRemove);
+            return blockasm.str();
+        },
+        "memory",
+        "free"
+    ),
+    SystemFunction(
+        [](const std::vector<Token>& params, int &nextAllocatedLocation, std::vector<Variable> &vars) -> std::string {
+            std::stringstream blockasm;
+            int indexToRename = -1;
+            for(int j = 0; j < vars.size(); j++) {
+                if(Variable var = vars[j]; var.name == params[0].children[0].value) {
+                    indexToRename = j;
+                    break;
+                }
+            }
+            if(indexToRename == -1) {
+                std::cerr << "Cannot set undefined variable." << std::endl;
+                exit(EXIT_FAILURE);
+            }
+            char* end;
+            int val = static_cast<int>(std::strtol(params[1].children[0].value.c_str(), &end, 10));
+            if(errno == ERANGE) {
+                std::cerr << "Expected integer as value" << std::endl;
+            }
+            blockasm << "SetCnst 0x" << std::setfill('0') << std::setw(8) << std::hex << vars[indexToRename].location << " 0x";
+            blockasm << std::setfill('0') << std::setw(16) << std::hex << val << " 0x00000000" << std::endl;
+            return blockasm.str();
+        },
+        "memory",
+        "set"
+    ),
+    SystemFunction(
+        [](const std::vector<Token>& params, int &nextAllocatedLocation, std::vector<Variable> &vars) -> std::string {
+            std::stringstream blockasm;
+            Signature sig = Signature({Type::uint64});
+            ParamsParser pp = ParamsParser(params, sig);
+            std::tuple<std::string, std::vector<int>> parsingResult = pp.ParseParams(nextAllocatedLocation, vars);
+            std::string expressionBlockasm = std::get<0>(parsingResult);
+            std::vector<int> locations = std::get<1>(parsingResult);
+            blockasm << expressionBlockasm << std::endl;
+            int dataLocation = locations[0];
+            blockasm << "Stdout 0x" << std::setfill('0') << std::setw(8) << std::hex << dataLocation << " 0x00000000" << std::endl;
+            return blockasm.str();
+        },
+        "io",
+        "print"
+    ),
+    SystemFunction(
+        [](const std::vector<Token>& params, int &nextAllocatedLocation, std::vector<Variable> &vars) -> std::string {
+            std::stringstream blockasm;
+            Signature sig = Signature({Type::uint64});
+            ParamsParser pp = ParamsParser(params, sig);
+            std::tuple<std::string, std::vector<int>> parsingResult = pp.ParseParams(nextAllocatedLocation, vars);
+            std::string expressionBlockasm = std::get<0>(parsingResult);
+            std::vector<int> locations = std::get<1>(parsingResult);
+            blockasm << expressionBlockasm << std::endl;
+            int dataLocation = locations[0];
+            blockasm << "Stderr 0x" << std::setfill('0') << std::setw(8) << std::hex << dataLocation << " 0x00000000" << std::endl;
+            return blockasm.str();
+        },
+        "io",
+        "err"
     )
 };
 
