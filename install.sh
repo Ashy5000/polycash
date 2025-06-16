755088ba835c07dd18af9b26ad68dceffd6eda9b
Ashy5000 <ashydashy5000@gmail.com>
2025-06-15 22:54:19 -0700

    Fixes install script to use updated build script

--- install.sh
+++ install.sh
@@ -1,9 +1,4 @@
 echo "Cloning repository..."
 git clone https://github.com/Ashy5000/polycash
 cd polycash
-echo "Building node software... ('consensus client')"
-go build -o builds/node/node
-echo "Building smart contract software... ('execution client')"
-cd contracts
-cargo build --release
-cd ..
+./build.sh
